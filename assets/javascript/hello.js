15e844870edf0d8db0f4fc36598198a0ab611b8e
nobuti <buti@nobuti.com>
2016-04-13 12:07:47 +0200

    Browsersync + browserify.
    TODO: HMR.

--- assets/javascript/hello.js
+++ assets/javascript/hello.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 class HelloWorld extends Component {
   render() {
-    return <h1>Hello {this.props.you} from React</h1>
+    return <h1>Hello {this.props.you}, from React</h1>
   }
 }
 
